794a4e26c7380e85c95fb319a97fb763a58be7a1
alvittoag <alvittogenz09@gmail.com>
2024-09-24 14:38:52 +0700

    feat: validation ganti password

--- app/(auth)/ganti-password.tsx
+++ app/(auth)/ganti-password.tsx
@@ -8,14 +8,20 @@ import { router } from "expo-router";
 import { ALERT_TYPE, Dialog } from "react-native-alert-notification";
 import { useMutation } from "@tanstack/react-query";
 import { axiosService } from "@/services/axiosService";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const gantiPasswordSchema = Yup.object().shape({
+  saatIni: Yup.string()
+    .min(2, "Minimum 2 Karakter")
+    .required("Harap Masukan Password Saat Ini"),
+  baru: Yup.string()
+    .min(2, "Minimum 2 Karakter")
+    .required("Harap Masukan Password Baru"),
+  konfirmasi: Yup.string().equals([Yup.ref("baru")], "Password Tidak Sama"),
+});
 
 export default function GantiPassword() {
-  const [password, setPassword] = React.useState({
-    saatIni: "",
-    baru: "",
-    konfirmasi: "",
-  });
-
   const [showPassword, setShowPassword] = React.useState({
     saatIni: false,
     baru: false,
@@ -23,13 +29,21 @@ export default function GantiPassword() {
   });
 
   const mutationEdit = useMutation({
-    mutationFn: async () => {
+    mutationFn: async ({
+      saatIni,
+      baru,
+      konfirmasi,
+    }: {
+      saatIni: string;
+      baru: string;
+      konfirmasi: string;
+    }) => {
       try {
         const res = await axiosService.put(
           "/api/auth-rest/user-reset-password",
           {
-            old_password: password.saatIni,
-            new_password: password.baru,
+            old_password: saatIni,
+            new_password: konfirmasi,
           }
         );
         return res.data;
@@ -43,12 +57,6 @@ export default function GantiPassword() {
         title: "Berhasil",
         textBody: "Password Berhasil Diubah",
         button: "Tutup",
-      });
-
-      setPassword({
-        saatIni: "",
-        baru: "",
-        konfirmasi: "",
       });
     },
     onError: (error) => {
@@ -70,27 +78,6 @@ export default function GantiPassword() {
     },
   });
 
-  const handleChangePassword = () => {
-    if (Object.values(password).includes("")) {
-      return Dialog.show({
-        type: ALERT_TYPE.DANGER,
-        title: "Gagal Ganti Password",
-        textBody: "Harap Isi Semua Data",
-        button: "Tutup",
-      });
-    }
-    if (password.baru !== password.konfirmasi) {
-      return Dialog.show({
-        type: ALERT_TYPE.DANGER,
-        title: "Gagal Ganti Password",
-        textBody: "Konfirmasi Password Tidak Sesuai",
-        button: "Tutup",
-      });
-    }
-
-    mutationEdit.mutate();
-  };
-
   return (
     <ContainerBackground>
       <View
@@ -100,118 +87,155 @@ export default function GantiPassword() {
           gap: moderateScale(25),
         }}
       >
-        <View style={{ gap: moderateScale(10) }}>
-          <TextInput
-            value={password.saatIni}
-            onChangeText={(e) =>
-              setPassword((prev) => ({ ...prev, saatIni: e }))
-            }
-            label="Password Saat ini"
-            mode="outlined"
-            secureTextEntry={!showPassword.saatIni}
-            style={{
-              backgroundColor: "white",
-              height: verticalScale(50),
-            }}
-            activeOutlineColor={Colors.border_input_active}
-            outlineColor={Colors.border_primary}
-            textColor="black"
-            right={
-              <TextInput.Icon
-                icon={showPassword.saatIni ? "eye-off" : "eye"}
-                onPress={() =>
-                  setShowPassword((prev) => ({
-                    ...prev,
-                    saatIni: !prev.saatIni,
-                  }))
-                }
-              />
-            }
-          />
+        <Formik
+          validationSchema={gantiPasswordSchema}
+          initialValues={{ saatIni: "", baru: "", konfirmasi: "" }}
+          onSubmit={({ baru, konfirmasi, saatIni }, { resetForm }) => {
+            mutationEdit.mutate({
+              saatIni,
+              baru,
+              konfirmasi,
+            });
+            resetForm();
+          }}
+        >
+          {({ handleChange, handleSubmit, values, errors }) => (
+            <>
+              <View style={{ gap: moderateScale(10) }}>
+                <TextInput
+                  value={values.saatIni}
+                  onChangeText={handleChange("saatIni")}
+                  error={errors.saatIni ? true : false}
+                  label="Password Saat ini"
+                  mode="outlined"
+                  secureTextEntry={!showPassword.saatIni}
+                  style={{
+                    backgroundColor: "white",
+                    height: verticalScale(50),
+                  }}
+                  activeOutlineColor={Colors.border_input_active}
+                  outlineColor={Colors.border_primary}
+                  textColor="black"
+                  right={
+                    <TextInput.Icon
+                      icon={showPassword.saatIni ? "eye-off" : "eye"}
+                      onPress={() =>
+                        setShowPassword((prev) => ({
+                          ...prev,
+                          saatIni: !prev.saatIni,
+                        }))
+                      }
+                    />
+                  }
+                />
 
-          <TextInput
-            label="Password Baru"
-            mode="outlined"
-            value={password.baru}
-            onChangeText={(e) => setPassword((prev) => ({ ...prev, baru: e }))}
-            secureTextEntry={!showPassword.baru}
-            style={{
-              backgroundColor: "white",
-              height: verticalScale(50),
-            }}
-            activeOutlineColor={Colors.border_input_active}
-            outlineColor={Colors.border_primary}
-            textColor="black"
-            right={
-              <TextInput.Icon
-                icon={showPassword.baru ? "eye-off" : "eye"}
-                onPress={() =>
-                  setShowPassword((prev) => ({ ...prev, baru: !prev.baru }))
-                }
-              />
-            }
-          />
+                {errors.saatIni && (
+                  <Text style={{ color: "salmon", fontWeight: "bold" }}>
+                    {errors.saatIni}
+                  </Text>
+                )}
 
-          <TextInput
-            label="Konfirmasi Password Baru"
-            mode="outlined"
-            value={password.konfirmasi}
-            onChangeText={(e) =>
-              setPassword((prev) => ({ ...prev, konfirmasi: e }))
-            }
-            secureTextEntry={!showPassword.konfirmasi}
-            style={{
-              backgroundColor: "white",
-              height: verticalScale(50),
-            }}
-            activeOutlineColor={Colors.border_input_active}
-            outlineColor={Colors.border_primary}
-            textColor="black"
-            right={
-              <TextInput.Icon
-                icon={showPassword.konfirmasi ? "eye-off" : "eye"}
-                onPress={() =>
-                  setShowPassword((prev) => ({
-                    ...prev,
-                    konfirmasi: !prev.konfirmasi,
-                  }))
-                }
-              />
-            }
-          />
-        </View>
+                <TextInput
+                  label="Password Baru"
+                  mode="outlined"
+                  value={values.baru}
+                  error={errors.baru ? true : false}
+                  onChangeText={handleChange("baru")}
+                  secureTextEntry={!showPassword.baru}
+                  style={{
+                    backgroundColor: "white",
+                    height: verticalScale(50),
+                  }}
+                  activeOutlineColor={Colors.border_input_active}
+                  outlineColor={Colors.border_primary}
+                  textColor="black"
+                  right={
+                    <TextInput.Icon
+                      icon={showPassword.baru ? "eye-off" : "eye"}
+                      onPress={() =>
+                        setShowPassword((prev) => ({
+                          ...prev,
+                          baru: !prev.baru,
+                        }))
+                      }
+                    />
+                  }
+                />
 
-        <View style={{ flexDirection: "row", gap: moderateScale(25) }}>
-          <Button
-            onPress={() => router.back()}
-            textColor="white"
-            icon={"arrow-left"}
-            style={{
-              backgroundColor: Colors.button_primary,
-              flex: 1,
-              paddingVertical: moderateScale(10),
-              borderRadius: 7,
-            }}
-          >
-            Kembali
-          </Button>
+                {errors.baru && (
+                  <Text style={{ color: "salmon", fontWeight: "bold" }}>
+                    {errors.baru}
+                  </Text>
+                )}
 
-          <Button
-            loading={mutationEdit.isPending}
-            disabled={mutationEdit.isPending}
-            labelStyle={{ color: "black" }}
-            onPress={handleChangePassword}
-            icon={"content-save-outline"}
-            style={{
-              backgroundColor: Colors.button_secondary,
-              flex: 1,
-              paddingVertical: moderateScale(10),
-              borderRadius: 7,
-            }}
-          >
-            Simpan
-          </Button>
-        </View>
+                <TextInput
+                  label="Konfirmasi Password Baru"
+                  mode="outlined"
+                  value={values.konfirmasi}
+                  onChangeText={handleChange("konfirmasi")}
+                  error={errors.konfirmasi ? true : false}
+                  secureTextEntry={!showPassword.konfirmasi}
+                  style={{
+                    backgroundColor: "white",
+                    height: verticalScale(50),
+                  }}
+                  activeOutlineColor={Colors.border_input_active}
+                  outlineColor={Colors.border_primary}
+                  textColor="black"
+                  right={
+                    <TextInput.Icon
+                      icon={showPassword.konfirmasi ? "eye-off" : "eye"}
+                      onPress={() =>
+                        setShowPassword((prev) => ({
+                          ...prev,
+                          konfirmasi: !prev.konfirmasi,
+                        }))
+                      }
+                    />
+                  }
+                />
+
+                {errors.konfirmasi && (
+                  <Text style={{ color: "salmon", fontWeight: "bold" }}>
+                    {errors.konfirmasi}
+                  </Text>
+                )}
+              </View>
+
+              <View style={{ flexDirection: "row", gap: moderateScale(25) }}>
+                <Button
+                  onPress={() => router.back()}
+                  textColor="white"
+                  icon={"arrow-left"}
+                  style={{
+                    backgroundColor: Colors.button_primary,
+                    flex: 1,
+                    paddingVertical: moderateScale(10),
+                    borderRadius: 7,
+                  }}
+                >
+                  Kembali
+                </Button>
+
+                <Button
+                  loading={mutationEdit.isPending}
+                  disabled={mutationEdit.isPending}
+                  labelStyle={{ color: "black" }}
+                  onPress={handleSubmit as any}
+                  icon={"content-save-outline"}
+                  style={{
+                    backgroundColor: Colors.button_secondary,
+                    flex: 1,
+                    paddingVertical: moderateScale(10),
+                    borderRadius: 7,
+                  }}
+                >
+                  Simpan
+                </Button>
+              </View>
+            </>
+          )}
+        </Formik>
       </View>
     </ContainerBackground>
   );
